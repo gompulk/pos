b410cf0cc320fc802044de8c5e891a9f3fce044d
gompulk <36143471+gompulk@users.noreply.github.com>
2026-04-13 15:48:30 +0700

    Add admin layout

---
+++ admin/layout.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Admin POS</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="d-flex">
+<div class="bg-dark text-white p-3" style="width:200px; height:100vh;">
+<h4>POS</h4>
+<a href="dashboard.php" class="text-white d-block">Dashboard</a>
+<a href="produk.php" class="text-white d-block">Produk</a>
+<a href="transaksi.php" class="text-white d-block">Transaksi</a>
+</div>
+<div class="p-4 w-100">
